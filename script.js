90492b835ba29e68ad56d55eb1bd9d2e6a7f23bd
Cauã de Lima <caua-l1@outlook.com>
2024-09-15 16:01:57 -0300

    update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let nomeUser
-
-function salvarNome(){
+function salvarNome(e){
     const inputNomeUser = document.getElementById('nome-usuario');
     const erroMsg = document.querySelector('.erro-nome')
 
     if (inputNomeUser.value){
-        nomeUser = inputNomeUser.value;
+        let nomeUser = inputNomeUser.value;
+        localStorage.setItem('NomeUser', nomeUser)
         erroMsg.style.display = 'none';
-        window.location.href  += 'menu.html'
+        window.location.href  = 'menu.html'
 
     } else {
+        e.preventDefault()
         erroMsg.style.display = 'block';
     };
 };
@@ -80,3 +80,11 @@ function esconderMenu(target){
         menuCombos.style.display = 'none';
     }
 }
+
+window.onload = function() {
+    var audio = document.getElementById('musica');
+    audio.play().catch(function(error) {
+        // Lida com o caso onde a reprodução automática não é permitida
+        console.log('Erro ao tentar tocar a música:', error);
+    });
+};
